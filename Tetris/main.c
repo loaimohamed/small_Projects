51deecc048e03539b6c4ca30d9431ddc05af41df
LoaiMA <loai_mohmed120@outlook.com>
2025-08-11 01:40:00 +0300

    still trying to rotate the shapes NOT DONE YET

--- Tetris/main.c
+++ Tetris/main.c
@@ -28,13 +28,12 @@ typedef struct {
     int id;
 } Shape_Struct;
 
-int Shapes[5][16] = {{3, 3, 3, 3, 1, 3, 3, 3, 1, 1, 3, 3, 1, 3, 3, 3},
+int Shapes[6][16] = {{3, 3, 3, 3, 1, 3, 3, 3, 1, 1, 3, 3, 1, 3, 3, 3},
     {3, 3, 1, 3, 1, 3, 1, 1},
     {3, 3, 3, 1, 1, 1, 1, 3},
     {1, 3, 1, 3, 1, 3, 1, 3},
-    {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 2}};
-
-int ShapeTest[4] = {3, 3, 1, 3};
+    {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 2},
+    {5, 5, 3, 3, 5, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3}};
 
 typedef enum {
     T_Shape = 0,
@@ -120,11 +119,48 @@ Point rotateA(Point p, Point d, Shape_Struct *ss) {
     return np;
 }
 
+void shapeRotate(Shape_Struct *ss) {
+    const int count = sizeof(Shapes[ss->id]) / sizeof(int);
+    int tempShape[count] = {};
+    const int hc = sqrt(count);
+    for (int i = 0; i < count; i++) {
+        tempShape[i] = Shapes[ss->id][i];
+        const int x = i % hc;
+        const int y = floor(i / hc);
+        int grid_pos = getCord(x, y, hc);
+
+        // printf("(%d, %d)", x + 2, y + 1);
+
+        Point og = {x, y};
+        Point pivot = {1.5, 1.5};
+        Point rotated = rotateA(og, pivot, ss);
+        Point np = {rotated.x + pivot.x + 0.5, rotated.y + pivot.y + 0.5};
+
+        int alt_pos = getCord(np.x, np.y, hc);
+
+        // printf("%d, %d\n", alt_pos, ShapeTest[alt_pos]);
+        // printf("%2.f, %2.f\n", rotated.x+pivot.x, rotated.y+pivot.y);
+        if (i == 4 || i == 8 || i == 9 || i == 12) {
+            // printf("<=");
+            // printf("(%d)(%d, %d) => (%2.f) (%f, %f) = (%d #%d)\n",
+            //     i, x, y,ss->angle, np.x, np.y, alt_pos, 
+            // (int)np.x
+            // );
+            // printf("%f, %f == %f, %f\n", rotated.x, rotated.y, pivot.x, pivot.y);
+        }
+        // printf("(%d)>(%.1f, %.1f)(%.1f) = %d\n",i, np.x, np.y,(double)hc, alt_pos);
+        // printf("%d>%d\n",i, alt_pos);
+        // printf("\n");
+        Shapes[ss->id][(int)alt_pos] = tempShape[grid_pos];
+    }
+}
+
 void setShapes(Grid *grid, Shape_Struct *ss) {
     grid->list[grid->spawnPoint] = 4;
-    const int count = sizeof(ShapeTest) / sizeof(int);
-    int *sh = ShapeTest;
-    const int hc = count / 2;
+    const int count = sizeof(Shapes[ss->id]) / sizeof(int);
+    // printf("%d\n", count);
+    int *sh = Shapes[ss->id];
+    const int hc = sqrt(count);
     for (int i = 0; i < count; i++) {
         const int x = i % hc;
         const int y = floor(i / hc);
@@ -132,59 +168,63 @@ void setShapes(Grid *grid, Shape_Struct *ss) {
         int grid_Ps = getCord(ip.x, ip.y, DIM);
         int Shape_Ps = getCord(x, y, hc);
 
-        Point np = rotateA(ip, ss->basePoint, ss);
-        Point np1 = {np.x + ss->basePoint.x, np.y + ss->basePoint.y};
-
-        // printf("%f %f\n", np1.x, np1.y);
-        int grid_Ps2 = getCord(np1.x, np1.y, DIM);
-
-        // grid->list[grid_Ps] = sh[Shape_Ps];
-        grid->list[grid_Ps2] = sh[Shape_Ps];
+        // printf("%f, %f = %d | %d\n", ip.x, ip.y, grid_Ps, sh[Shape_Ps]);
+        grid->list[grid_Ps] = sh[Shape_Ps];
+        // if (i == 4 || i == 8 || i == 9 || i == 12) grid->list[grid_Ps] = 5;
     }
 }
 
 void FrameUpdate(Grid *grid, Shape_Struct *ss) {
+    // shapeRotate(ss);
     setShapes(grid, ss);
     display(grid);
+    const Point oldPoint = ss->basePoint;
     for (;;) {
         // if (kbhit()) {
-            int key = getch();
-            if (key == 0 || key == 224) key = getch();
-            if (key == 3) break;
+        int key = getch();
+        if (key == 0 || key == 224) key = getch();
+        if (key == 3) break;
 
-            clearGrid(grid);
-            ClearScreen();
-            setShapes(grid, ss);
-            display(grid);
-
-            switch (key) {
-                case Direc_UP:
-                    setShapes(grid, ss);
-                    break;
-                case Direc_Down:
-                    if (ss->basePoint.y < DIM) ss->basePoint.y++;
-                    break;
-                case Direc_Left:
-                    break;
-                case Direc_Right:
-                    break;
-            }
+        switch (key) {
+            case Direc_UP:
+                // ss->angle = 90; 
+                shapeRotate(ss);
+                break;
+            case Direc_Down:
+                if (ss->basePoint.y + 4 < DIM) ss->basePoint.y++;
+                break;
+            case Direc_Left:
+                if (ss->basePoint.x >= 0) ss->basePoint.x--;
+                break;
+            case Direc_Right:
+                if (ss->basePoint.x + 4 < DIM) ss->basePoint.x++;
+                break;
+            case 114:
+                ss->basePoint = oldPoint;
+                break;
+            default:
+                printf("Not Assigned Key: %c (%d)", key, key);
+                break;
+        }
+        ClearScreen();
+        clearGrid(grid);
+        setShapes(grid, ss);
+        display(grid);
         // _sleep(50);
         // }
     }
 }
 
-
 int main() {
     Grid grid = {.length = DIM * DIM, .spawnPoint = 60};
     grid.list = (int[DIM * DIM]){};
-    Debug = 1;
+    // Debug = 1;
     // int i = 2;
 
     Point center = {3.5, 3.5};
 
     Shape_Struct ss = {
-        .basePoint = {2, 2}, .angle = 0, .pivot = center, .id = F_Shape};
+        .basePoint = {2, 2}, .angle = 90, .pivot = center, .id = T_Shape};
 
     FrameUpdate(&grid, &ss);
 
